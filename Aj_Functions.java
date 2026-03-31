cdaec724aa746263e6eb3c7910259ed805d5830f
KoshtiHarshal <koshtiharshal51@gmail.com>
2026-03-31 22:26:44 +0530

    Java D3

--- Aj_Functions.java
+++ Aj_Functions.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.util.function.IntUnaryOperator;
 public class Aj_Functions {
     public static void printMyName(String name){
         System.out.println("Hello, " + name);
