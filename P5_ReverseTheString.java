5cbc12084fd4007d6a1d6ed64dac4e0ba6dc90a8
KoshtiHarshal <koshtiharshal51@gmail.com>
2026-04-22 18:34:12 +0530

    JAVA D12

---
+++ P5_ReverseTheString.java
@@ -0,0 +1,16 @@
+public class P5_ReverseTheString{
+    public static void printRev(String str , int idx){
+        if ( idx == 0){
+            System.out.println(str.charAt(idx));
+            return;
+        }
+        System.out.print(str.charAt(idx));
+        printRev(str, idx-1);
+    }
+    public static void main(String args[]){
+        String str = "ABCD";
+        printRev(str, str.length()-1);        
+    }
+}
+
+// Time complexity : O(n)
