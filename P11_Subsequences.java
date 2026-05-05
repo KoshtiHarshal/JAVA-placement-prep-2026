e7104579603cd3043db1f2c8cbdc1c263f138f34
KoshtiHarshal <koshtiharshal51@gmail.com>
2026-05-05 19:20:42 +0530

    JAVA D16

---
+++ P11_Subsequences.java
@@ -0,0 +1,17 @@
+// P : Print all the subsequences of a string.
+
+public class P11_Subsequences{
+    public static void subsequences(String str, int idx, String newstr){
+        if(idx == str.length()){
+            System.out.println(newstr);
+            return;
+        }
+        subsequences(str, idx + 1, newstr + str.charAt(idx));
+
+        subsequences(str, idx + 1, newstr);
+    }
+    public static void main(String args[]){
+        String str = "abc";
+        subsequences(str, 0, "");
+    }
+}
