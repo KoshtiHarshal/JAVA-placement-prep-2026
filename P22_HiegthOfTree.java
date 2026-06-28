f32f30cf0e0573bcdf85e875f1df6f88802a49eb
KoshtiHarshal <koshtiharshal51@gmail.com>
2026-06-28 14:24:02 +0530

    JAVA D34

---
+++ P22_HiegthOfTree.java
@@ -0,0 +1,51 @@
+// P : Hieght of Tree.
+
+public class P22_HiegthOfTree {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }   
+    }
+
+    static class BinaryTree {
+        static int idx = -1;
+        public static Node buildTree(int nodes[]){
+            idx++;
+            if(nodes[idx] == -1){
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+        }
+    }
+
+    public static int hieghtOfTree(Node root){
+        if (root == null) {
+            return 0;
+        }
+
+        int leftHieght = hieghtOfTree(root.left);
+        int rightHieght = hieghtOfTree(root.right);
+
+        int hieght = Math.max(leftHieght,rightHieght) + 1;
+
+        return hieght;
+    }
+    public static void main(String args[]){
+        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        Node root = BinaryTree.buildTree(nodes);
+
+        System.out.println(hieghtOfTree(root));
+    }
+}
+
+// Time Complexity : O(n)
+// Space Complexity : O(1)
